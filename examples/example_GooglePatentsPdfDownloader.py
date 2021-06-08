6438283dddc89f165940400bb0ba91866466353b
lorenzbr <46646302+lorenzbr@users.noreply.github.com>
2021-06-08 16:50:18 +0200

    argument file changed

--- examples/example_GooglePatentsPdfDownloader.py
+++ examples/example_GooglePatentsPdfDownloader.py
@@ -7,7 +7,7 @@ from GooglePatentsPdfDownloader.GooglePatentsPdfDownloader import GooglePatentsP
 download = GooglePatentsPdfDownloader(verbose = True)
 
 download.get_pdf(patent = "US4405829A1", output_path = "", driver_file = "chromedriver.exe")
-download.get_pdfs(patents = ["US4405829A1", "EP0551921B1"], file = None, output_path = "", 
+download.get_pdfs(patents = ["US4405829A1", "EP0551921B1"], file = "", output_path = "", 
                   driver_file = "chromedriver.exe", remove_kind_codes = ["A1"])
 download.get_pdfs(patents = None, file = "docs/data/patents.txt", output_path = "", 
                   driver_file = "chromedriver.exe", remove_kind_codes = ["A1"])
