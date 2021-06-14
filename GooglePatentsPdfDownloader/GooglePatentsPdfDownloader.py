86052607220876f966679dc068b584667be62e74
lorenzbr <46646302+lorenzbr@users.noreply.github.com>
2021-06-14 14:49:42 +0200

    documentation

--- GooglePatentsPdfDownloader/GooglePatentsPdfDownloader.py
+++ GooglePatentsPdfDownloader/GooglePatentsPdfDownloader.py
@@ -14,6 +14,26 @@ class GooglePatentsPdfDownloader:
         self.verbose = verbose    
     
     def get_pdf(self, patent, output_path = "", driver_file = "chromedriver.exe", waiting_time = 6, remove_kind_codes = None):
+        """ Download a patent document as a PDF        
+
+        Parameters
+        ----------
+        patent : str
+            A string containing a patent number (e.g., US4405829A1, EP0551921B1).
+        output_path : str, optional
+            An output path where documents are saved. The default is "".
+        driver_file : str, optional
+            Path and file name of the Chrome driver exe. The default is "chromedriver.exe".
+        waiting_time : int, optional
+            Waiting time in seconds for each request. The default is 6.
+        remove_kind_codes : list, optional
+            A list containing the patent kind codes which should be removed from patent numbers. The default is None.
+
+        Returns
+        -------
+        None.
+
+        """
        
         if remove_kind_codes is not None:
             for remove_kind_code in remove_kind_codes:
@@ -57,7 +77,34 @@ class GooglePatentsPdfDownloader:
         except:
             print("Error: Download link for patent " + patent + " not found!")
         
-    def get_pdfs(self, patents = None, file = "", output_path = "", driver_file = "chromedriver.exe", waiting_time = 6, remove_kind_codes = None):        
+    def get_pdfs(self, patents = None, file = "", output_path = "", driver_file = "chromedriver.exe", waiting_time = 6, remove_kind_codes = None):
+        """ Download several patents as PDF documents        
+
+        Parameters
+        ----------
+        patents : list, optional
+            A list containing patent numbers (e.g., US4405829A1, EP0551921B1). The default is None.
+        file : str, optional
+            A string containing the path and file name. The needs to have one column with patent numbers. 
+            The column name has to be patent_number. The default is "".
+        output_path : str, optional
+            An output path where documents are saved. The default is "".
+        driver_file : str, optional
+            Path and file name of the Chrome driver exe. The default is "chromedriver.exe".
+        waiting_time : int, optional
+            Waiting time in seconds for each request. The default is 6.
+        remove_kind_codes : list, optional
+            A list containing the patent kind codes which should be removed from patent numbers. The default is None.
+
+        Returns
+        -------
+        None.
+        
+        Examples
+        ----------
+        See https://github.com/lorenzbr/GooglePatentsPdfDownloader#readme
+
+        """
                 
         if file != "":
             df_patents = pd.read_csv(file)
