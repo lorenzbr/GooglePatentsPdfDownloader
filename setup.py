70e3a2423bd97f1f45eac326a92cd5f44bd0bf8f
lorenzbr <46646302+lorenzbr@users.noreply.github.com>
2021-06-23 13:50:35 +0200

    install requires

--- setup.py
+++ setup.py
@@ -7,14 +7,16 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='GooglePatentsPdfDownloader',
-    version='0.1.0',
-    description='Download patents as PDF documents from Google Patents',
-    long_description=readme,
-    author='Lorenz Brachtendorf',
-    author_email='lorenz.brachtendorf@gmx.de',
-    url='https://github.com/lorenzbr/GooglePatentsPdfDownloader',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    name = 'GooglePatentsPdfDownloader',
+    version = '0.1.0',
+    description = 'Download patents as PDF documents from Google Patents',
+    long_description = readme,
+    author = 'Lorenz Brachtendorf',
+    author_email = 'lorenz.brachtendorf@gmx.de',
+    url = 'https://github.com/lorenzbr/GooglePatentsPdfDownloader',
+    license = license,
+    packages = find_packages(exclude = ('tests', 'docs')),
+    install_requires = ['requests', 'bs4', 'pandas', 'selenium', 'numpy'],
+    include_package_data = True
 )
 
