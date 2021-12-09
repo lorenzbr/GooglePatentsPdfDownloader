3b2b967508521e23ad2ce5d0d820d7bcd17db274
stthpast <thomas.m.pastuska@student.hs-anhalt.de>
2021-12-09 15:01:29 +0100

    make patent_downloader.py more PEP8 like

--- GooglePatentsPdfDownloader/patent_downloader.py
+++ GooglePatentsPdfDownloader/patent_downloader.py
@@ -1,20 +1,28 @@
+from typing import List, Union, Optional
 import requests
+import time
+import re
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time
-import re
 
-class patent_downloader:
+
+class PatentDownloader:
     
     url = "https://patents.google.com"
     
-    def __init__(self, verbose = False):
+    def __init__(self, verbose=False):
         self.verbose = verbose    
     
-    def get_pdf(self, patent, output_path = "", driver_file = "chromedriver.exe", waiting_time = 6, remove_kind_codes = None):
-        """ Download a patent document as a PDF        
+    def get_pdf(
+            self,
+            patent: str,
+            output_path: str = "",
+            driver_file: str = "chromedriver.exe",
+            waiting_time: int = 6,
+            remove_kind_codes: Optional[List[str]] = None):
+        """ Download a patent document as a PDF
 
         Parameters
         ----------
@@ -32,61 +40,64 @@ class patent_downloader:
         Returns
         -------
         None.
-
         """
-       
-        if remove_kind_codes is not None:
+        if remove_kind_codes:
             for remove_kind_code in remove_kind_codes:
                 patent = re.sub(remove_kind_code + "$", "", patent)
         
-        ## get selenium started and open url
-        driver = webdriver.Chrome(executable_path = driver_file)
+        # get selenium started and open url
+        driver = webdriver.Chrome(executable_path=driver_file)
         driver.get(self.url)
     
-        element = driver.find_element_by_name('q')
+        # element = driver.find_element_by_name('q')
+        element = driver.find_element('q')
         element.send_keys(patent)
         element.send_keys(Keys.RETURN)
         
-        ## wait X secs
+        # wait X secs
         time.sleep(waiting_time)
                 
-        ## get html code for page with expanded tree of recommendations
+        # get html code for page with expanded tree of recommendations
         raw_html = driver.page_source
         
-        ## close browser
+        # close browser
         driver.quit()
         
-        ## parse html code from that webpage
+        # parse html code from that webpage
         soup = BeautifulSoup(raw_html, 'html.parser')
-           
-        ## get specific part of html code
-        soup2 = soup.find_all('a', class_ = 'style-scope patent-result')
-        
-        
-        try:            
-            ## identify PDF link in html source code
+
+        # get specific part of html code
+        soup2 = soup.find_all('a', class_='style-scope patent-result')
+
+        try:
+            # identify PDF link in html source code
             pdf_link = soup2[0].attrs['href']
             
-            ## download PDF
+            # download PDF
             myfile = requests.get(pdf_link)
-            open(output_path + patent + '.pdf', 'wb').write(myfile.content)
+            open(f'{output_path}{patent}.pdf', 'wb').write(myfile.content)
             
-            ## print statement
-            print('Patent ' + patent + ' successfully downloaded')
+            # print statement
+            print(f'Patent {patent} successfully downloaded')
             
         except:
-            print("Error: Download link for patent " + patent + " not found!")
+            print(f'Error: Download link for patent {patent} not found!')
         
-    def get_pdfs(self, patents = None, file = "", output_path = "", driver_file = "chromedriver.exe", waiting_time = 6, remove_kind_codes = None):
+    def get_pdfs(
+            self,
+            patents: Union[List[str], str],
+            output_path: str = "",
+            driver_file: str = "chromedriver.exe",
+            waiting_time: int = 6,
+            remove_kind_codes: Optional[List[str]] = None):
         """ Download several patents as PDF documents        
 
         Parameters
         ----------
-        patents : list, optional
-            A list containing patent numbers (e.g., US4405829A1, EP0551921B1). The default is None.
-        file : str, optional
-            A string containing the path and file name. The needs to have one column with patent numbers. 
-            The column name has to be patent_number. The default is "".
+        patents : list, str
+            Ether a list containing patent numbers (e.g., US4405829A1, EP0551921B1),
+            or a string containing the path and file name (CSV,TXT).
+            The CSV needs a column named `patent_number`.
         output_path : str, optional
             An output path where documents are saved. The default is "".
         driver_file : str, optional
@@ -103,24 +114,23 @@ class patent_downloader:
         Examples
         ----------
         See https://github.com/lorenzbr/GooglePatentsPdfDownloader#readme
-
         """
-                
-        if file != "":
-            df_patents = pd.read_csv(file)
-            patents = list(df_patents['patent_number'])
+        if isinstance(patents, str):
+            if patents.lower().endswith('csv'):
+                df_patents = pd.read_csv(patents)
+                patents = df_patents['patent_number'].to_list()
+            elif patents.lower().endswith('txt'):
+                with open(patents, 'r') as txt_file:
+                    patents = txt_file.read().splitlines()
+            else:
+                raise NotImplementedError(f'Unsupported file type: {patents}')
 
-        total = len(patents)        
-        
-        i = 1
-        
-        for patent in patents:
-            
-            self.get_pdf(patent = patent, output_path = output_path, driver_file = driver_file, 
-                         waiting_time = waiting_time, remove_kind_codes = remove_kind_codes)
-
-            remaining = total - i
-
-            i += 1
-            
-            print(str(remaining) + " patent(s) remaining.")
+        for i, patent in enumerate(patents):
+            self.get_pdf(
+                patent=patent,
+                output_path=output_path,
+                driver_file=driver_file,
+                waiting_time=waiting_time,
+                remove_kind_codes=remove_kind_codes
+            )
+            print(patents.size - i, " patent(s) remaining.")
