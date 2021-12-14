8d5929b0e32e303d7ae1b9a1a2e1c985f36fe26e
stthpast <thomas.m.pastuska@student.hs-anhalt.de>
2021-12-15 00:56:39 +0100

    edit/update example file

--- README.md
+++ README.md
@@ -55,16 +55,16 @@ patent_downloader.download(patent="EP0551921B1")
 
 # Download multiple patents using a list of inputs to the current working directory
 patent_downloader.download(
-    patents=["US4405829A1", "EP0551921B1", "EP1304824B1"],
+    patent=["US4405829A1", "EP0551921B1", "EP1304824B1"],
     output_path="./pdf_files",
     remove_kind_codes=["A1"]
 )
 
 # Download multiple patents using a txt file to the current working directory
 patent_downloader.download(
-    patents="docs/data/patents.txt", 
-    output_path = "",
-    remove_kind_codes = ["A1"]
+    patent="docs/data/patents.txt", 
+    output_path="",
+    remove_kind_codes=["A1"]
 )
 ```
 
--- examples/example_GooglePatentsPdfDownloader.py
+++ examples/example_GooglePatentsPdfDownloader.py
@@ -1,18 +1,24 @@
 ## Download PDF documents of patents from Google Patents
 ## URL: https://patents.google.com
 
-from GooglePatentsPdfDownloader.patent_downloader import patent_downloader
-patent_downloader = patent_downloader(verbose = True)
+from GooglePatentsPdfDownloader import PatentDownloader
+patent_downloader = PatentDownloader(chrome_driver='chromedriver.exe', app='brave')
 
 # Download a single patent to the current working directory (not found w/ kind code)
-patent_downloader.get_pdf(patent = "US4405829A1", output_path = "", driver_file = "chromedriver.exe")
-patent_downloader.get_pdf(patent = "US4405829", output_path = "", driver_file = "chromedriver.exe")
+patent_downloader.download(patent="US4405829A1", remove_kind_codes=['A1'])
+patent_downloader.download(patent="EP0551921B1")
 
-# Download multiple patents from list of inputs to the current working directory
-patent_downloader.get_pdfs(patents = ["US4405829A1", "EP0551921B1", "EP1304824B1"],
-                           output_path = "", driver_file = "chromedriver.exe", 
-                           remove_kind_codes = ["A1"])
 
-# Download multiple patents from txt file to the current working directory
-patent_downloader.get_pdfs(file = "docs/data/patents.txt", output_path = "",
-                           driver_file = "chromedriver.exe", remove_kind_codes = ["A1"])
+# Download multiple patents using a list of inputs to the current working directory
+patent_downloader.download(
+    patent=["US4405829A1", "EP0551921B1", "EP1304824B1"],
+    output_path="./pdf_files",
+    remove_kind_codes=["A1"]
+)
+
+# Download multiple patents using a txt file to the current working directory
+patent_downloader.download(
+    patent="../docs/data/patents.txt",
+    output_path="",
+    remove_kind_codes=["A1"]
+)
