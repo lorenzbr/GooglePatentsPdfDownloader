f8b09ced1ffa9e247ae2588d30a07b5aa33dfade
stthpast <thomas.m.pastuska@student.hs-anhalt.de>
2021-12-15 00:24:48 +0100

    re-wrote PatentDownloader, fixed not-found PDF link, added Brave as option, simplified download by a new method, moved chromedrive argument to constructor

--- GooglePatentsPdfDownloader/patent_downloader.py
+++ GooglePatentsPdfDownloader/patent_downloader.py
@@ -1,7 +1,8 @@
 from typing import List, Union, Optional
+import os
 import requests
-import time
 import re
+import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,109 +13,110 @@ class PatentDownloader:
     
     url = "https://patents.google.com"
     
-    def __init__(self, verbose=False):
-        self.verbose = verbose    
-    
-    def get_pdf(
-            self,
-            patent: str,
-            output_path: str = "",
-            driver_file: str = "chromedriver.exe",
-            waiting_time: int = 6,
-            remove_kind_codes: Optional[List[str]] = None):
-        """ Download a patent document as a PDF
+    def __init__(self, chrome_driver: str = 'chromedriver.exe', app: Optional[str] = None, verbose: bool = False):
+        """
 
         Parameters
         ----------
-        patent : str
-            A string containing a patent number (e.g., US4405829A1, EP0551921B1).
+        chrome_driver : str
+            Path and file name of the Chrome driver exe. Default is "chromedriver.exe".
+        app : str, optional
+            Change Chrome application for Brave by passing 'brave'. Default is None.
+        """
+        self.verbose = verbose  # TODO: unused attribute?
+        self.driver_file = chrome_driver
+        self.option = None
+        if app == 'brave':
+            brave_path = brave_application_path()
+            self.option = webdriver.ChromeOptions()
+            self.option.binary_location = brave_path
+            self.option.add_argument("--incognito")
+
+    def download(self,
+                 patent: Union[str, List[str]],
+                 output_path: str = "./",
+                 waiting_time: int = 6,
+                 remove_kind_codes: Optional[List[str]] = None) -> None:
+        """Download patent document(s) as PDF
+
+        Parameters
+        ----------
+        patent : str, list[str]
+            Patent(s) to download.
+            Ether a string containing a patent number (e.g., US4405829A1, EP0551921B1),
+            or a list containing patent numbers,
+            or a string containing the path and file name (CSV,TXT). The CSV needs a column named `patent_number`.
         output_path : str, optional
-            An output path where documents are saved. The default is "".
-        driver_file : str, optional
-            Path and file name of the Chrome driver exe. The default is "chromedriver.exe".
+            An output path where documents are saved. Default is "./".
         waiting_time : int, optional
             Waiting time in seconds for each request. The default is 6.
         remove_kind_codes : list, optional
-            A list containing the patent kind codes which should be removed from patent numbers. The default is None.
+            A list containing the patent kind codes which should be removed from patent numbers. Default is None.
 
         Returns
         -------
         None.
+
+        Examples
+        ----------
+        See https://github.com/lorenzbr/GooglePatentsPdfDownloader#readme
         """
+        try:
+            valid_path = os.path.isfile(patent)
+        except TypeError:
+            valid_path = False
+
+        if valid_path or isinstance(patent, list):
+            self.get_pdfs(
+                patents=patent,
+                output_path=output_path,
+                waiting_time=waiting_time,
+                remove_kind_codes=remove_kind_codes
+            )
+        else:
+            self.get_pdf(
+                patent=patent,
+                output_path=output_path,
+                waiting_time=waiting_time,
+                remove_kind_codes=remove_kind_codes
+            )
+
+    def get_pdf(self, patent: str, output_path: str = "./", waiting_time: int = 6,
+                remove_kind_codes: Optional[List[str]] = None) -> None:
+
         if remove_kind_codes:
             for remove_kind_code in remove_kind_codes:
                 patent = re.sub(remove_kind_code + "$", "", patent)
         
         # get selenium started and open url
-        driver = webdriver.Chrome(executable_path=driver_file)
+        if self.option:
+            driver = webdriver.Chrome(executable_path=self.driver_file, options=self.option)
+        else:
+            driver = webdriver.Chrome(executable_path=self.driver_file)
         driver.get(self.url)
     
-        # element = driver.find_element_by_name('q')
-        element = driver.find_element('q')
+        element = driver.find_element_by_name('q')
         element.send_keys(patent)
         element.send_keys(Keys.RETURN)
-        
-        # wait X secs
-        time.sleep(waiting_time)
-                
-        # get html code for page with expanded tree of recommendations
-        raw_html = driver.page_source
-        
-        # close browser
-        driver.quit()
+        time.sleep(waiting_time)  # wait X secs
+        raw_html = driver.page_source  # get html code for page with expanded tree of recommendations
+        driver.quit()  # close driver
         
         # parse html code from that webpage
         soup = BeautifulSoup(raw_html, 'html.parser')
+        pdf_link = self.get_pdf_link(soup, patent)
 
-        # get specific part of html code
-        soup2 = soup.find_all('a', class_='style-scope patent-result')
+        if pdf_link:
+            path_prefix = os.path.abspath(output_path)
+            patent_file = requests.get(pdf_link)
+            with open(os.path.join(path_prefix, f'{patent}.pdf'), 'wb') as pdf_file:
+                pdf_file.write(patent_file.content)
+            print(f'>>> Patent {patent} successfully downloaded <<<')  # print statement
+        else:
+            pass
 
-        try:
-            # identify PDF link in html source code
-            pdf_link = soup2[0].attrs['href']
-            
-            # download PDF
-            myfile = requests.get(pdf_link)
-            open(f'{output_path}{patent}.pdf', 'wb').write(myfile.content)
-            
-            # print statement
-            print(f'Patent {patent} successfully downloaded')
-            
-        except:
-            print(f'Error: Download link for patent {patent} not found!')
-        
-    def get_pdfs(
-            self,
-            patents: Union[List[str], str],
-            output_path: str = "",
-            driver_file: str = "chromedriver.exe",
-            waiting_time: int = 6,
-            remove_kind_codes: Optional[List[str]] = None):
-        """ Download several patents as PDF documents        
-
-        Parameters
-        ----------
-        patents : list, str
-            Ether a list containing patent numbers (e.g., US4405829A1, EP0551921B1),
-            or a string containing the path and file name (CSV,TXT).
-            The CSV needs a column named `patent_number`.
-        output_path : str, optional
-            An output path where documents are saved. The default is "".
-        driver_file : str, optional
-            Path and file name of the Chrome driver exe. The default is "chromedriver.exe".
-        waiting_time : int, optional
-            Waiting time in seconds for each request. The default is 6.
-        remove_kind_codes : list, optional
-            A list containing the patent kind codes which should be removed from patent numbers. The default is None.
-
-        Returns
-        -------
-        None.
-        
-        Examples
-        ----------
-        See https://github.com/lorenzbr/GooglePatentsPdfDownloader#readme
-        """
+    def get_pdfs(self, patents: Union[List[str], str], output_path: str = "./", waiting_time: int = 6,
+                 remove_kind_codes: Optional[List[str]] = None) -> None:
         if isinstance(patents, str):
             if patents.lower().endswith('csv'):
                 df_patents = pd.read_csv(patents)
@@ -129,8 +131,33 @@ class PatentDownloader:
             self.get_pdf(
                 patent=patent,
                 output_path=output_path,
-                driver_file=driver_file,
                 waiting_time=waiting_time,
                 remove_kind_codes=remove_kind_codes
             )
-            print(patents.size - i, " patent(s) remaining.")
+            print(len(patents) - i+1, "patent(s) remaining.")
+
+    @staticmethod
+    def get_pdf_link(soup: BeautifulSoup, patent: str):
+        pdfs: List[str] = [link['href'] for link in soup.find_all('a', href=True)
+                           if link['href'].lower().endswith('pdf')]
+        for pdf in pdfs:
+            if patent.lower() in pdf.lower():  # TODO: ignore kind code?
+                return pdf  # return first matching pdf link
+            else:
+                continue
+        print(f'Error: Download link for patent {patent} not found!')
+        return None
+
+
+def brave_application_path() -> str:
+    win_paths = [
+        r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe',
+        r'C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe',
+    ]
+    # TODO: add paths
+    for win_path in win_paths:
+        if os.path.isfile(win_path):
+            return win_path
+        else:
+            pass
+    raise FileNotFoundError
